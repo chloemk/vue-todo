ed48ab786b8cf4bc23f1cf716d3fb56a5e84271e
chloemk <minsung.chloe.kim@gmail.com>
2021-11-28 19:37:35 +0900

    Feat: add basic styling for headers and page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,42 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+	<main class="main-wrapper">
+		<h1 class="page-title">TODO-LIST</h1>
+		<p class="subtitle">Let's get things done!</p>
+		<input type="text" />
+		<button>+ Add</button>
+	</main>
 </template>
 
+<script setup></script>
+
 <style>
+html {
+	background-color: #fbfbfb;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Source Code Pro', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	font-size: 16px;
+}
+
+.main-wrapper {
+	max-width: 600px;
+	margin: 0 auto;
+}
+
+.page-title {
+	font-size: 44px;
+	color: #2d2d2d;
+	font-weight: 600;
+	margin-bottom: 10px;
+	margin-top: 150px;
+}
+
+.subtitle {
+	font-size: 1rem;
+	font-weight: bold;
+	color: #6b6b6b;
+	margin-top: 0;
 }
 </style>

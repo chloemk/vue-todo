e843dd3f758f0fbb6415b7876e25ce8da1f62ec9
chloemk <minsung.chloe.kim@gmail.com>
2021-11-30 22:03:50 +0900

    Add: Task Input comp created

---
+++ src/components/TaskInput.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="new-task-wrapper">
+		<input
+			v-model="newInput"
+			@keyup.enter="addTodo"
+			class="new-task-input"
+			type="text"
+			placeholder="Type here to create"
+		/>
+		<button @click="addTodo" class="new-task-button">+ Add</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function () {
+		return {
+			newInput: '',
+		};
+	},
+	methods: {
+		addTodo: function () {
+			console.log('인풋 값', this.newInput);
+			localStorage.setItem(this.newInput, this.newInput);
+			this.newInput = ''; //클릭이 되면 비워주기
+		},
+	},
+};
+</script>
+
+<style>
+.new-task-wrapper {
+	display: flex;
+}
+
+.new-task-input {
+	padding: 16px;
+	color: #959595;
+	border: none;
+	flex: 1;
+	font-weight: bold;
+	border-top-left-radius: 7px;
+	border-bottom-left-radius: 7px;
+	box-shadow: 2px 4px 5px 1px rgba(0, 0, 0, 0.23);
+	letter-spacing: 0.8px;
+}
+
+.new-task-input:focus {
+	outline: none;
+}
+
+.new-task-button {
+	padding: 18px 24px;
+	background-color: #e8b4b8;
+	color: #fff;
+	font-weight: bold;
+	border-top-right-radius: 7px;
+	border-bottom-right-radius: 7px;
+	border: none;
+}
+</style>
